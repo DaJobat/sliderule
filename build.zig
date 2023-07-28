3b3674590f0ab9ee281b783f5b8b0380db307f4f
jobat <jobat@toolbelt-games.com>
2023-07-28 15:57:20 +0100

    module update

--- build.zig
+++ build.zig
@@ -1,11 +1,23 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const lib = b.addModule("sliderule", .{
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    _ = b.addModule("sliderule", .{
         .source_file = std.Build.FileSource.relative("sliderule.zig"),
     });
 
-    const tests = b.addTest(.{ .root_source_file = lib.source_file });
+    const lib = b.addStaticLibrary(.{
+        .name = "sliderule",
+        .root_source_file = std.Build.FileSource.relative("sliderule.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    b.installArtifact(lib);
+
+    const tests = b.addTest(.{ .root_source_file = std.Build.FileSource.relative("sliderule.zig") });
 
     const run_tests = b.addRunArtifact(tests);
     const test_step = b.step("test", "run tests");
